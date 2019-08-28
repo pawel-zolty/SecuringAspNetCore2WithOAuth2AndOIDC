01253846892cee24d45df9aebae7cb9c3b9867d3
Paweł Żółtaniecki <pzoltaniecki@insert.com.pl>
2019-08-28 16:50:05 +0200

    Get claims info about authed user (from User Endpoint)

--- Starter Files/ImageGallery/src/ImageGallery.Client/Startup.cs
+++ Starter Files/ImageGallery/src/ImageGallery.Client/Startup.cs
@@ -44,6 +44,7 @@ namespace ImageGallery.Client
                options.Scope.Add("profile");
                options.SaveTokens = true;
                options.ClientSecret = "secret";
+               options.GetClaimsFromUserInfoEndpoint = true;
            });
 
             // my services
