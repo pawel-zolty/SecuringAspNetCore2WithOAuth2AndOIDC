c1548d2bc10804b6fd75838a28b409cc8edea2df
Paweł Żółtaniecki <pzoltaniecki@insert.com.pl>
2019-08-28 13:10:38 +0200

    add test config and test data

--- Starter Files/ImageGallery/src/Zoltaniecki.IDP/Startup.cs
+++ Starter Files/ImageGallery/src/Zoltaniecki.IDP/Startup.cs
@@ -15,6 +15,11 @@ namespace Zoltaniecki.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryClients(Config.GetClients())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +30,7 @@ namespace Zoltaniecki.IDP
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
