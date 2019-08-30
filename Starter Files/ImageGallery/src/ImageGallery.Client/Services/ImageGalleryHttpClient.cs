2a05f5a5785fe5816f5f9db694551b0a30b63a16
Paweł Żółtaniecki <pzoltaniecki@insert.com.pl>
2019-08-30 15:08:21 +0200

    fix 401 on calling api and pass token with every call to api

--- Starter Files/ImageGallery/src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
+++ Starter Files/ImageGallery/src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -17,7 +19,19 @@ namespace ImageGallery.Client.Services
         }
         
         public async Task<HttpClient> GetClient()
-        {      
+        {
+            string accessToken = string.Empty;
+            var currentContext = _httpContextAccessor.HttpContext;
+
+            accessToken = await currentContext
+                .GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                //set Bearer token
+                _httpClient.SetBearerToken(accessToken);
+            }
+
             _httpClient.BaseAddress = new Uri("https://localhost:44339/");
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(
